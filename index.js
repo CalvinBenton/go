e824b94405b33316e1fe5107c21a9ecc2f5ae179
Calvin <calvinbenton@hotmail.co.uk>
2017-01-28 21:18:21 +0000

    logbody

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var Tour = require('./tours.js');
 
+const Speech = require('@google-cloud/speech');
+
+function syncRecognize (filename) {
+  const speech = Speech();
+
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRate: 16000
+  };
+
+  return speech.recognize(filename, config)
+    .then((results) => {
+      const transcription = results[0];
+      console.log(`Transcription: ${transcription}`);
+      return transcription;
+    });
+}
+
 module.exports = function (app) {
 
     app.get('/tours', function(req, res) {
@@ -10,12 +28,14 @@ module.exports = function (app) {
     });
 
     app.post('/tours', function(req, res) {
-        var obj = new Tour(req.body);
-        obj.save(function(err, obj) {
-            if(err) return console.error(err);
-            console.log(obj);
-            res.status(200).json(obj);
-        });
+        console.log(req.body);
+    });
+
+    app.get('/hotel', function(req, res) {
+        app.get('http://partners.api.skyscanner.net/apiservices/hotels/autosuggest/v2/UK/EUR/en-GB/pari?apikey=prtl6749387986743898559646983194', function(req, res) {
+        console.log(res)
+
+    });
     });
 
 
