593dccc7c9f88791d7f22fb8cf1bf511c429cbf5
Calvin <calvinbenton@hotmail.co.uk>
2017-01-28 17:48:19 +0000

    routs

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var Tour = require('./tours.js');
 module.exports = function (app) {
 
     app.get('/tours', function(req, res) {
-        Tour.findOne({}, function (err, obj) {
+        Tour.find({}, function (err, obj) {
             if(err) return console.error(err);
             res.json(obj);
         })
     });
 
+    app.post('/tours', function(req, res) {
+        var obj = new Tour(req.body);
+        obj.save(function(err, obj) {
+            if(err) return console.error(err);
+            res.status(200).json(obj);
+        });
+    });
 
 };
