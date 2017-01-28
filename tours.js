5f1e067a6a4e44cccae5dc9413991a0444a470e7
Calvin <calvinbenton@hotmail.co.uk>
2017-01-28 17:38:47 +0000

    longi lati

--- tours.js
+++ tours.js
@@ -3,7 +3,13 @@
 var mongoose = require('mongoose');
 
 var tourSchema = mongoose.Schema({
-    name: String
+    longi: Number,
+    lati: Number,
+    audioUrl: String,
+    createdAt: Number,
+    rating: Number,
+    noOfRatings: Number
+
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
